4baebcff82923a29c9ca4ed8612db998cd6ef226
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-01-21 22:44:50 +0000

    rust snake

---
+++ snake/src/main.rs
@@ -0,0 +1,43 @@
+extern crate piston_window;
+extern crate rand;
+extern crate sdl2_window;
+
+mod draw;
+mod game;
+mod snake;
+
+use piston_window::types::Color;
+use piston_window::*;
+use sdl2_window::Sdl2Window;
+
+use draw::to_coord_u32;
+use game::Game;
+
+const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+
+fn main() {
+    let (width, height) = (20, 20);
+
+    let mut window: PistonWindow<Sdl2Window> =
+        WindowSettings::new("Snake", [to_coord_u32(width), to_coord_u32(height)])
+            .exit_on_esc(true)
+            .build()
+            .unwrap();
+
+    let mut game = Game::new(width, height);
+
+    while let Some(event) = window.next() {
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            game.key_pressed(key);
+        }
+
+        window.draw_2d(&event, |c, g| {
+            clear(BACK_COLOR, g);
+            game.draw(&c, g);
+        });
+
+        event.update(|arg| {
+            game.update(arg.dt);
+        });
+    }
+}
